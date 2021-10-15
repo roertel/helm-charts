7390cdb1aee9fbcfd01a825aec19fd9aa682c563
Ryan Oertel <ryan@integra.net>
2021-10-15 13:57:47 -0500

    Updated scripts

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
-CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/main -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
-KUBEVAL_VERSION="0.14.0"
+CHART_DIRS="$(git diff --find-renames --name-only \
+  "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/main -- charts \
+  | grep '[cC]hart.yaml' \
+  | sed -e 's#/[Cc]hart.yaml##g')"
+KUBEVAL_VERSION="0.16.1"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/main/"
 
 # install kubeval
-curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
+curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz \
+  https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
 tar -xf /tmp/kubeval.tar.gz kubeval
 
 # validate charts
