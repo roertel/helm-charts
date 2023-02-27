43a8783392a9663d1e2c1f16c79ad3fc24420514
Ryan Oertel <638327+roertel@users.noreply.github.com>
2023-02-27 08:44:01 -0600

    updated commit hook & doc generation

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -2,15 +2,18 @@
 set -euo pipefail
 
 HELM_DOCS_VERSION="1.11.0"
+PATH=${PATH}:.
 
 # install helm-docs
-curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz \
-  https://github.com/norwoodj/helm-docs/releases/download/v"${HELM_DOCS_VERSION}"/helm-docs_"${HELM_DOCS_VERSION}"_Linux_x86_64.tar.gz
-tar -xf /tmp/helm-docs.tar.gz helm-docs
+if ! which helm-docs 2>/dev/null; then
+  curl --silent --show-error --fail --location \
+    https://github.com/norwoodj/helm-docs/releases/download/v"${HELM_DOCS_VERSION}"/helm-docs_"${HELM_DOCS_VERSION}"_Linux_x86_64.tar.gz \
+  | tar x helm-docs
+fi
 
 # validate docs (Generate "gold standard", then compare against pushed version)
-./helm-docs --log-level error
-if find . -name README.md|xargs git diff --stat --exit-code; then
+helm-docs --log-level error --sort-values-order file --document-dependency-values
+if find . -name README.md -print0 | xargs --null git diff --stat --exit-code; then
   echo "Helm docs linting passed"
 else
   echo "ERROR: Run helm-docs linting failed. Re-run helm-docs before committing."
